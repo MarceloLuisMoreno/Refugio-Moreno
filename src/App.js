e0e90f20c05ddf7801aa5bf37faef33bd6d1d53e
Marcelo Luis Moreno <marceloluismoreno@gmail.com>
2021-10-19 09:18:03 -0300

    Desafío Crea tu Landing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import ItemListContainer from "./components/Containers/ItemListContainer";
 import NavBar from "./components/NavBar";
 
 function App() {
-   return (
-      <div className="App">
-         <header className="App-header">
-            <NavBar />
-         </header>
-      </div>
-   );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <NavBar />
+                <ItemListContainer greeting="Bienvenidos a Refugio - Tienda Deco" />
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,35 +1,58 @@
+//Component NavBar
+
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import CardWidget from "./CardWidget";
 
 function NavBar() {
    return (
-      <div>
-         <Navbar expand="lg" bg="dark" variant="dark">
-            <Container>
-               <Navbar.Brand href="#home">
-                  <img
-                     src="/refugio.png"
-                     className="d-inline-block align-top"
-                     alt="Refugio Tienda Deco logo"
-                  />
-               </Navbar.Brand>
-               <Nav className="me-auto">
-                  <Nav.Link href="#home">Home</Nav.Link>
-                  <Nav.Link href="#nosotros">Nosotros</Nav.Link>
-                  <Nav.Link href="#fotos">Fotos</Nav.Link>
-                  <NavDropdown title="Productos" id="basic-nav-dropdown">
-                     <NavDropdown.Item href="#cuadros">Cuadros</NavDropdown.Item>
-                     <NavDropdown.Item href="#macetas">Macetas</NavDropdown.Item>
-                     <NavDropdown.Item href="#plantas">Plantas</NavDropdown.Item>
-                  </NavDropdown>
-               </Nav>
-            </Container>
-         </Navbar>
-         <br />
-      </div>
-   );
+        <div>
+            <Navbar
+                collapseOnSelect
+                expand="lg"
+                bg="dark"
+                variant="dark"
+                fixed="top"
+            >
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <img
+                        src="/refugio.png"
+                        className="d-inline-block align-top"
+                        alt="Refugio Tienda Deco logo"
+                    />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#nosotros">Nosotros</Nav.Link>
+                        <Nav.Link href="#fotos">Fotos</Nav.Link>
+                        <NavDropdown title="Productos" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#cuadros">
+                            Cuadros
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#macetas">
+                            Macetas
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#plantas">
+                            Plantas
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+                <Nav>
+                    <Nav.Link href="#carrito">
+                        <CardWidget cantidad='0' />
+                    </Nav.Link>
+                </Nav>
+                </Container>
+            </Navbar>
+            <br />
+        </div>
+    );
 }
 
 export default NavBar;
