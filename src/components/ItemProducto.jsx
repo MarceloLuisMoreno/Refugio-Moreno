46c453ed6c1528ef1583a78b4017158f5ff87d1a
Marcelo Luis Moreno <marceloluismoreno@gmail.com>
2021-10-23 20:50:25 -0300

    Desafío: Contador con botón

--- src/components/ItemProducto.jsx
+++ src/components/ItemProducto.jsx
@@ -4,23 +4,6 @@ import { Container, Row, Col } from "react-bootstrap";
 
 function ItemProducto({ stock, initial, onAdd }) {
 	return (
-		/* 		<div className="containerItem">
-			<img
-				src="\assets\productos\planta-1.jpg"
-				className="containerImagenItem"
-				alt="Producto de Refugio Tienda Deco productos"
-			/>
-			<div className="container">
-				<br /><br />
-				<h3>Aloe</h3>
-				<h4>Planta en Maceta de cemento con plato</h4>
-				<h3>$ 950.-</h3>
-				<br /><br />
-				<ItemCount stock={stock} initial={initial} onAdd={onAdd} />
-			</div>
-		</div>
- */
-
 		<div>
 			<Container>
 				<Row className="gx-5">
