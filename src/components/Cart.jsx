33e70bf0a84b8621d5ea55c24a20241da583cfd2
Marcelo Luis Moreno <marceloluismoreno@gmail.com>
2021-11-15 20:08:49 -0300

    Desafío: Item Collection

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,7 @@
 import {useCartContext} from "../context/CartContext";
 import {Button, Container} from "react-bootstrap";
 import {Link} from "react-router-dom";
+import Flip from "react-reveal";
 
 function Cart() {
 	const {cartList, itemsCarrito, totalCart, clear, clearItem} = useCartContext();
@@ -10,11 +11,16 @@ function Cart() {
 			{itemsCarrito() === 0 ? (
 				<Container>
 					<br />
-					<h1 className="mt-5" >El carrito está vacío...</h1>
+					<Flip top>
+						<h1 className="mt-5">El carrito está vacío...</h1>
+					</Flip>
 					<br />
 					<video loop autoPlay>
-                        <source src="/assets/videos/carritoVacio.mp4" type="video/mp4" alt="Marketing illustrations by Storyset" ></source>
-                    </video>
+						<source
+							src="/assets/videos/carritoVacio.mp4"
+							type="video/mp4"
+							alt="Marketing illustrations by Storyset"></source>
+					</video>
 					<br />
 					<Button as={Link} to="/category/todos" variant="success" className="mx-2">
 						Hacé tu COMPRA!!!
@@ -23,10 +29,10 @@ function Cart() {
 			) : (
 				<div>
 					<Container>
-					< >
-						<br />
-						<h1 className="mt-2 p-4">Carrito de compras</h1>
-						<table className="table table-bordered table-primary text-center" >
+						<>
+							<br />
+							<h1 className="mt-2 p-4">Carrito de compras</h1>
+							<table className="table table-bordered table-primary text-center">
 								<thead>
 									<tr className="fs-5 fw-bold">
 										<th>Id.</th>
@@ -37,63 +43,67 @@ function Cart() {
 										<th>Eliminar</th>
 									</tr>
 								</thead>
-							<tbody>
-								{cartList.map((prod) => (
-									<tr key={prod.id}>
-										<td>{prod.id}</td>
-										<td>{prod.quantity}</td>
-										<td>{prod.name}</td>
-										<td>
-											{Intl.NumberFormat("es-AR", {
-												currency: "ARS",
-												style: "currency",
-											}).format(prod.price)}
-										</td>
-										<td>
-											{Intl.NumberFormat("es-AR", {
-												currency: "ARS",
-												style: "currency",
-											}).format(prod.quantity * prod.price)}
-										</td>
-										<td
-											className="bi bi-trash-fill"
-											onClick={() => clearItem(prod.id)}></td>
-									</tr>
-								))}
-							</tbody>
-						</table>
-						<br></br>
-						<h5>{`Totales de unidades del carrito: ${itemsCarrito()} -  Importe Total de la Compra ${Intl.NumberFormat(
-							"es-AR",
-							{
-								currency: "ARS",
-								style: "currency",
-							}
-						).format(totalCart())}`}</h5>
+								<tbody>
+									{cartList.map((prod) => (
+										<tr key={prod.id}>
+											<td>{prod.id}</td>
+											<td>{prod.quantity}</td>
+											<td>{prod.name}</td>
+											<td>
+												{Intl.NumberFormat("es-AR", {
+													currency: "ARS",
+													style: "currency",
+												}).format(prod.price)}
+											</td>
+											<td>
+												{Intl.NumberFormat("es-AR", {
+													currency: "ARS",
+													style: "currency",
+												}).format(prod.quantity * prod.price)}
+											</td>
+											<td
+												className="bi bi-trash-fill"
+												onClick={() => clearItem(prod.id)}></td>
+										</tr>
+									))}
+								</tbody>
+							</table>
+							<br></br>
+							<h5>{`Totales de unidades del carrito: ${itemsCarrito()} -  Importe Total de la Compra ${Intl.NumberFormat(
+								"es-AR",
+								{
+									currency: "ARS",
+									style: "currency",
+								}
+							).format(totalCart())}`}</h5>
 
-						<br />
-						<h6>Medios de Pago:</h6>
-						<img
-							src={"/assets/mediosPago/mediosPago.png"}
-							alt="distintos medios de pago"
-							className="w-50 m-3"
-						/>
-						<br />
-						<Button as={Link} to="/category/todos" variant="success" className="mx-2">
-							Seguir comprando
-						</Button>
-						<Button as={Link} to="/" variant="success" className="mx-2">
-							Finalizar compra
-						</Button>
-						<Button
-							onClick={() => {
-								clear();
-							}}
-							variant="success"
-							className="mx-2">
-							Vaciar carrito
-						</Button>
-						<br />
+							<br />
+							<h6>Medios de Pago:</h6>
+							<img
+								src={"/assets/mediosPago/mediosPago.png"}
+								alt="distintos medios de pago"
+								className="w-50 m-3"
+							/>
+							<br />
+							<Button
+								as={Link}
+								to="/category/todos"
+								variant="success"
+								className="mx-2">
+								Seguir comprando
+							</Button>
+							<Button as={Link} to="/" variant="success" className="mx-2">
+								Finalizar compra
+							</Button>
+							<Button
+								onClick={() => {
+									clear();
+								}}
+								variant="success"
+								className="mx-2">
+								Vaciar carrito
+							</Button>
+							<br />
 						</>
 					</Container>
 				</div>
--- src/components/Containers/HomeContainer.jsx
+++ src/components/Containers/HomeContainer.jsx
@@ -1,14 +1,17 @@
-import {Container} from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import Zoom from "react-reveal";
 
-function HomeContainer({greeting}) {
+function HomeContainer({ greeting }) {
 	return (
 		<Container>
 			<h2 className="text-center pt-5 mt-5">{greeting}</h2>
-			<img
-				src="assets/img/productos.png"
-				className="container pt-5"
-				alt="Refugio Tienda Deco productos"
-			/>
+			<Zoom>
+				<img
+					src="assets/img/productos.png"
+					className="container pt-5"
+					alt="Refugio Tienda Deco productos"
+				/>
+			</Zoom>
 			<h2 className="text-center p-3">Objetos de Diseño para tu casa</h2>
 			<br />
 			<br />
